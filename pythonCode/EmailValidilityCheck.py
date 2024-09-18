c039a71e8dcf86f6c79e183d4f8c9c8ef49ca53a
davidstanchak <19stanchakD@centralfoundationboys.co.uk>
2024-09-18 09:51:00 +0000

    restart

--- pythonCode/EmailValidilityCheck.py
+++ pythonCode/EmailValidilityCheck.py
@@ -1,14 +1,5 @@
+import re
 email = input("Enter your email adress: ")
-validAt = False
-validDot = False
-while validAt == False and validDot == False:
-    for i in range(0,len(email)):
-        if email[i] == "@":
-            validAt = True
-        elif email[i] == ".":
-            validDot = True
 
-if validAt == True and validDot == True:
-    print("Email valid")
-else:
-    print("Email invalid")
+if re.match(r"^(.*?)@(.*?).(.*?)",email):
+    print("Valid")
