fb89b1d350abcdc83b5c033d41a5f1dbc89d8822
davidstanchak <19stanchakD@centralfoundationboys.co.uk>
2024-09-18 10:00:10 +0000

    B4 break

--- pythonCode/EmailValidilityCheck.py
+++ pythonCode/EmailValidilityCheck.py
@@ -1,5 +1,7 @@
 import re
 email = input("Enter your email adress: ")
 
-if re.match(r"^(.*?)@(.*?).(.*?)",email):
+if re.match(r"^[a-zA-Z0-9]+@(.*?).[a-zA-Z]+",email):
     print("Valid")
+else:
+    print("Invalid")
