851725aa89de952a4ea080015f0736694b11ff6d
davidstanchak <19stanchakD@centralfoundationboys.co.uk>
2024-09-18 11:06:41 +0000

    SubmittedVersion

--- pythonCode/passwordStrength.py
+++ pythonCode/passwordStrength.py
@@ -1,9 +1,10 @@
 import re
-passwordValid = True
-while passwordValid != False:
+passwordValid = False
+while passwordValid == False:
     password = str(input("Create a password: "))
-    if re.match(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\w)",password) and len(password) < 8:
+    if re.match(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\w)",password) and len(password) >= 8:
         print("Password Valid")
+        passwordValid = True
     else:
         print("Password Invalid")
         passwordValid = False
