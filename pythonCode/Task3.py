8d2af09e2a398f35315b543012db56ea15c0d516
davidstanchak <19stanchakD@centralfoundationboys.co.uk>
2024-09-24 11:08:40 +0000

    12:11

--- pythonCode/Task3.py
+++ pythonCode/Task3.py
@@ -1,30 +1,41 @@
-user_ID = davidstanchak
+user_ID = "davidstanchak"
 user_PIN = 2837
 
 
 entry = False
 while entry == False:
-    attempt_PIN = input("Enter your 4-digit pin")
-    if len(attempt_PIN) == 4 and attempt_PIN.isdigit():
-        print(attempt_PIN, " is valid")
-        correct_PIN = True
-    else:
-        correct_PIN = False
-    attempt_ID = input("Enter your 10-character user ID")
-    
-    if len(attempt_ID) >= 10 and type(attempt_ID) == str:
-        compare_ID = True
+    check_length_again = True
+    while check_length_again == True:
 
-        print(attempt_PIN, " is valid")
-        correct_ID = True
+        attempt_PIN = input("Enter your 4-digit pin: \n")
+        if len(attempt_PIN) == 4 and attempt_PIN.isdigit():
+            correct_PIN = True
+        elif len(attempt_PIN) != 4:
+            check_length_again = True
+            
+        else:
+            correct_PIN = False
+        check_length_again = False
 
-        if correct_PIN == True and correct_ID == True:
-            print("Valid, access permitted")
-            entry = True
-        
+        attempt_ID = input("Enter your user ID which is greater than 10 characters: \n")
+    check_length_again == True
+    while check_length_again == True:
+
+        if len(attempt_ID) >= 10 and type(attempt_ID) == str:
+            correct_ID = True
 
         else:
-            print("Incorrect ID or PIN - Try again")
+            check_length_again = True
+        check_length_again = True
+            
+
+
+
+    if correct_PIN == True and correct_ID == True:
+        print("Valid, access permitted")
+        entry = True
+    else:
+        print("Incorrect ID or PIN - Try again")
             
 
 
