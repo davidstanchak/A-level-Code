bd241a11639d215b5677634c076665ac02e5b0bd
davidstanchak <19stanchakD@centralfoundationboys.co.uk>
2024-09-26 13:47:23 +0000

    SamHelpedKinda

--- pythonCode/Task3Redo.py
+++ pythonCode/Task3Redo.py
@@ -2,75 +2,49 @@ import random
 id_attempts = 0
 
 password = "helloworld"
-pin = 4545
+pin = "4545"
 userID = "mynameisdavidhello"
-def login(userID,pin,password):
+def login(userID, pin, password):
     attempts = 0
     max_attempts = 3
     while attempts < max_attempts:
-        userID_enter = input("Enter you 10-character user ID: \n") 
-        attempts += 1
-        if userID_enter == userID:
-            attempts = 0
-            max_attempts = 1
-            while attempts < max_attempts:
-                pin_enter = input("Please enter your pin: ")
-                if pin_enter == pin:
-                    attempts = 0
-                    max_attempts = 3
-                    while attempts < max_attempts:
-                        print("TEMP") # TODO: implement functionallity
-                else:
-                    print("Access denied.")
-        elif attempts == max_attempts:
-            print("Access denied")
+        userID_input = input("Enter you 10 character user id: ")
+        if userID_input == userID:
+            print("Correct user id")
+            break
+        else:
+            attempts += 1
+            if attempts < max_attempts:
+                print(f"Incorrect user id. You only have {max_attempts - attempts} attempts left.")
+            else:
+                print("Incorrect user id. Access denied.")
+                return
+            
+    pin_input = input("Enter your pin: ")
+    if pin_input != pin:
+        print(f"{pin_input} {pin}")
+        print("Access denied. No more attempts left.")
+        return
+        
+    attempts = 0
+    while attempts < max_attempts:
+        positions = random.sample(range(1, len(password) + 1), 3)
+        correct = True
+        for pos in positions:
+            password_char_input = input(f"Enter character {pos} of your password: ")
+            if password_char_input != password[pos-1]:
+                correct = False
+                break
+        if correct:
+            print("Access granted.")
             return
         else:
-            print(f"User ID incorrect {max_attempts - attempts} attempts left")
+            attempts += 1
+            if attempts < max_attempts:
+                print(f"Incorrect password. You only have {max_attempts - attempts} attempts left")
+            else:
+                print("Incorrect password.")
+                return
 
-        # PIN_attemps = 0
-        # userPIN_enter = input("Enter your 4-digit PIN: \n")
-        # PIN_attemps += 1
-        # if userPIN_enter == pin:
-        #     second = True
-        #     PIN_attemps = 3
-        # random_attemps = 0
-        # while random_attemps < 3:
-        #     guess_one = False
-        #     guess_two = False
-        #     guess_three = False
-        #     while guess_one != True and guess_two != True and guess_three != True:
-        #         first_random = random.randint(1, len(password))
-        #         first_guess = input("enter character place", str(first_random) ," of your password. \n")
-        #         if first_guess == password[first_random]:
-        #             guess_one = True
-                
-        #         second_random = first_random
-        #         while second_random == first_random:
-        #             second_random = random.randint(len(password))
-        #         second_guess = input("enter character place", second_random," of your password.")
-                
-        #         if second_guess == password[second_random]:
-        #             guess_two = True
-        #         third_random = random.randint(len(password))
 
-        #         third_guess = str(input("enter character place", third_random," of your password."))
-        #         third_random = random.randint(len(password))
-        #         third_random = second_random
-        #         while third_random == second_random:
-        #             third_random = random.randint(len(password))
-        #         if second_guess == password[second_random]:
-        #             guess_three = True
-        #         random_attemps = 4
-        #         if guess_one == True and guess_two == True and guess_three == True:
-        #             third = True
-
-            
-        #     third = True
-    
-    # if first == True and second == True and third == True:
-    #     print("access granted ")
-    # else:
-    #     print("access denied")
-
-login(userID,pin,password)
+login(userID, pin, password)
