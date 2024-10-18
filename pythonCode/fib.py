c9e1282bf2f6d53af4a293706d22fa04dea97814
davidstanchak <19stanchakD@centralfoundationboys.co.uk>
2024-10-18 12:29:29 +0000

    finalBothArrays

---
+++ pythonCode/fib.py
@@ -0,0 +1,2 @@
+def fibonacci2(n):
+    
--- pythonCode/moorse.py
+++ pythonCode/moorse.py
@@ -20,59 +20,12 @@ for i in range(len(user_sentence_split)):
         all_letters.append(NewLetterList)
 
 def alphabet_checker(check):
-    if check == "a":
-        moorse = "•-"
-    elif check == "b":
-        moorse = "-•••"
-    elif check == "c":
-        moorse = "-•-•"
-    elif check == "d":
-        moorse = "-••"
-    elif check == "e":
-        moorse = "•"
-    elif check == "f":
-        moorse = "••-•"
-    elif check == "g":
-        moorse = "--•"
-    elif check == "h":
-        moorse = "••••"
-    elif check == "i":
-        moorse = "••"
-    elif check == "j":
-        moorse = "•---"
-    elif check == "k":
-        moorse = "-•-"
-    elif check == "l":
-        moorse = "•-••"
-    elif check == "m":
-        moorse = "--"
-    elif check == "n":
-        moorse = "-•"
-    elif check == "o":
-        moorse = "---"
-    elif check == "p":
-        moorse = "•--•"
-    elif check == "q":
-        moorse = "--•-"
-    elif check == "r":
-        moorse = "•-•"
-    elif check == "s":
-        moorse = "•••"
-    elif check == "t":
-        moorse = "-"
-    elif check == "u":
-        moorse = "••-"
-    elif check == "v":
-        moorse = "•••-"
-    elif check == "w":
-        moorse = "•--"
-    elif check == "x":
-        moorse = "-••-"
-    elif check == "y":
-        moorse = "-•--"
-    elif check == "z":
-        moorse = "--••"
-    return moorse
+    a = [chr(i) for i in range(97, 123)]
+    m = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..']
+    for i in range(1,24):
+        if a[i-1] == check:
+            moorse = m[i]
+            return moorse
     
 for a in range(len(all_letters)): #there are "how many" arrays/words in the 2d array/sentence
     for b in range(len(all_letters[a])): #checks how many letters there are in total
