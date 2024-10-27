2fbddf991e788657b6db2511633c0b0a34dd3dc5
davidstanchak <19stanchakD@centralfoundationboys.co.uk>
2024-10-27 23:21:18 +0000

    starting eighths possible method

--- pythonCode/Leet_MostWater.py
+++ pythonCode/Leet_MostWater.py
@@ -7,15 +7,67 @@
 import random
 import numpy
 
-'''n = random.randint(2, 10**5)'''
-n = 20 #here is the hashtag you muppet
+#n = random.randint(2, 10**5)
+n = 20
 bars = random.choices(range(0, 10**4), k=n)
-midpoint = n//2
-half_areas = [None] * n
+'''
+midpoint = n//2 #truncated 
+half_areas = [None] * n #creating array with contents 'None' and length 'n' for memory efficiency
 for i in range(n):
     half_areas[i] = bars[i]*(midpoint-i)
         
 print(half_areas)
+'''
+'''
+eighth = n//8
+sectionIndex, n_Increment = 0, 0
+sectioned_Bars = ['e'] * n
+section_Length = len(sectioned_Bars[sectionIndex])
+
+while (sectionIndex < 8) and (n_Increment != section_Length):
+
+    section_Length = len(sectioned_Bars[sectionIndex])
+
+    sectioned_Bars = numpy.reshape(sectioned_Bars,(sectionIndex,section_Length))
+    
+
+    for i in range(eighth):
+        if i != eighth:
+            sectioned_Bars[sectionIndex,i] = bars[n_Increment]
+            n_Increment +=1
+        else:
+            sectionIndex+=1
+
+    sectioned_Bars = numpy.reshape(sectioned_Bars,(sectionIndex,n_Increment))
+
+    for i in reversed(range(section_Length)):
+        if i != -1:
+            sectioned_Bars[(7-sectionIndex),i] = bars[n-(section_Length*sectionIndex)]
+            n_Increment +=1
+        else:
+            sectionIndex+=1
+    print(sectioned_Bars)'''
+
+    #sort_sectioned_Bars = sectioned_Bars[sectionIndex].sort(reverse=True)
+
+    
+    
+
+'''
+
+    n_increment+=1
+print(sectioned_Bars)
+
+
+for i in range(eighth):
+
+right_first_quarter_array = bars[eighth]
+left_first_quarter_array.sort(reverse=True)
+for i in range(eighth):
+'''
+
+
+
 
 
 
