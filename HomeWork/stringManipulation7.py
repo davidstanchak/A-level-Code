5066b7454310b714bc08ebd781f23ce4937c33f4
davidstanchak <19stanchakD@centralfoundationboys.co.uk>
2024-11-11 20:43:52 +0000

    firstcommit

---
+++ HomeWork/stringManipulation7.py
@@ -0,0 +1,14 @@
+email = "19stanchakD@centralfoundationboys.co.uk"
+end = False
+x=1
+domain = []
+while end != True:
+    if email[x-1] == "@":
+        domain.append(email[x])
+    x+=1
+print(domain.lower)
+
+            
+        
+    
+    
--- pythonCode/ClassBankACC.py
+++ pythonCode/ClassBankACC.py
@@ -1,1 +1,15 @@
 class Bank:
+    __bankAccounts = {"Samantha Hey":"000001"}
+    def __init__(self, accountName, accountNumber):
+        self.__bankAccounts = dict(self.__bankAccounts, accountName=accountNumber)
+    
+    def getAccount(self, bankAccounts):
+        return bankAccounts.get("Bob Shni")
+
+
+
+myName = input("Enter Name")
+myAccNum = input("Enter acc number")
+newAccount = Bank(myName, myAccNum)
+print(Bank.getAccount())
+
