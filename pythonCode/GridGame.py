155a456daf5f1b8e0bca3428c1ba97fc3f836efa
David Stanchak <19stanchakD@centralfoundationboys.co.uk>
2025-01-15 11:51:05 +0000

    final

---
+++ pythonCode/GridGame.py
@@ -0,0 +1,12 @@
+grid = [
+    ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
+    ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
+    ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
+    ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
+    ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
+    ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x']
+]
+x,y = int(input("Enter x")), int(input("Enter y"))
+grid[y-1][x-1] = "O"
+for row in grid:
+    print(' '.join(row))
---
+++ pythonCode/pg12SalesQuarters.py
@@ -0,0 +1,5 @@
+Outlet1Sales = [10,12,15,10]
+Outlet2Sales = [5,8,3,6]
+Outlet3Sales = [10,12,15,10]
+for i in range(4):
+    print(f"Total for quarter {i+1} {Outlet1Sales[i]+Outlet2Sales[i]+Outlet3Sales[i]}")
