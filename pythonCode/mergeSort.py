beda46e7edc5323a026b6cde643780c65356221d
David Stanchak <19stanchakD@centralfoundationboys.co.uk>
2025-12-05 14:26:18 +0000

    mergecopied

--- pythonCode/mergeSort.py
+++ pythonCode/mergeSort.py
@@ -1,5 +1,50 @@
-numbers = [38, 27, 43, 3, 9, 82, 10]
+def merge_sort(items):
 
-left = 0
-right = len(numbers)
+    # Base case for recursion:
+    # The recursion will stop when the list has been divided into single items
+    if len(items) <= 1:
+        return items
+    else:
+        midpoint = (len(items)-1) // 2 # Calculate the midpoint index
+        left_half = items[0:midpoint+1] # Create left half list
+        right_half = items[midpoint+1:len(items)] # Create right half list
 
+        left_half = merge_sort(left_half) # Recursive call on left half
+        right_half = merge_sort(right_half) # Recursive call on right half
+        
+        # Call procedure to merge both halves
+        merged_items = merge(left_half, right_half) # Call function to merge both halves
+
+        return merged_items
+    
+def merge(left, right):
+
+    merged = [] # New list for merging the items
+    index_left = 0 # left current position
+    index_right = 0 # right current position
+
+    # While there are still items to merge
+    while index_left < len(left) and index_right < len(right):
+
+        # Find the lowest of the two items being compared 
+        # and add it to the new list
+        if left[index_left] < right[index_right]:
+            merged.append(left[index_left])
+            index_left += 1
+        else:
+            merged.append(right[index_right])
+            index_right += 1
+
+    # Add to the merged list any remaining data from left list
+    while index_left < len(left):
+        merged.append(left[index_left])
+        index_left += 1
+
+    # Add to the merged list any remaining data from right list
+    while index_right < len(right):
+        merged.append(right[index_right])
+        index_right += 1
+
+    return merged
+
+print(merge_sort([7, 12, 25, 33, 45, 18, 4, 29, 50, 21]))
