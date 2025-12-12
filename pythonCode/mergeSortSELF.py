975b790b8fb3078c2360b1bbdfd486a04497f176
David Stanchak <19stanchakD@centralfoundationboys.co.uk>
2025-12-12 09:52:10 +0000

    merge

--- pythonCode/mergeSortSELF.py
+++ pythonCode/mergeSortSELF.py
@@ -1,17 +1,25 @@
-my_List = [3,5,1,7,0,9,11]
+my_list = [6,3,7,0,1,4]
 
+def list_split(items):
 
-def list_split(my_List):
-    left_list = my_List[:(len(my_List))]
-    right_list = my_List[(len(my_List)):]
+    if len(items) <= 1:
+        return items
+    midpoint = (len(items) - 1)//2
+    left_list = items[:midpoint+1]
+    right_list = items[midpoint+1:len(items)]
 
     left_list = list_split(left_list)
     right_list = list_split(right_list)
 
-    mergedList = merge_sort(left_list,right_list)
-    return mergedList
+    # mergedList = merge_sort(left_list,right_list)
+    merged_items = merge_sort(left_list,right_list)
 
-def merge_sort():
+    return merged_items
+
+print(list_split(my_list))
+
+# print(list_split(my_List))
+def merge_sort(left,right):
 
     merged = [] 
     index_left = 0 
